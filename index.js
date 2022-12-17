5f9694bda7e30af7b1cd48bc8c004e63d2f15e9e
ttaki <opqrshun@gmail.com>
2022-12-17 10:06:40 +0900

    delete bot token

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ const openApiSearch = async (text) => {
  * SocketMode
  */
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   customRoutes: [
     {
